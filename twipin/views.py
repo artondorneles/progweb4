460faaf6c6ace27fb5eea0a8ef8ef4bb5f0d286a
Arton Dorneles <artondorneles@gmail.com>
2018-10-22 20:45:27 -0200

    Quase funcionando a curtida

--- twipin/views.py
+++ twipin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
-from .models import Twip
+from .models import Twip,Curtida
 from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
@@ -16,17 +16,39 @@ def api_twips_id(request, id):
 
       jt = {"mensagem": "ok"}
       return JsonResponse(jt)
-    
+
+@csrf_exempt
+def api_coracao(request, id):
+
+    c = 0 #número de curtidas
+
+    if request.method == "POST":
+      t = Twip.objects.get(id=id)
+      u = User.objects.get(username='admin')
+      curtida = Curtida(autor=u, twip=t)
+      curtida.save()
+      c = t.curtida_set.count()
+    elif request.method == "DELETE":
+      t = Twip.objects.get(id=id)
+      u = User.objects.get(username='admin')
+      curtida = Curtida.objects.get(autor=u, twip=t)
+      curtida.delete()
+      c = t.curtida_set.count()
+
+    jt = {"mensagem": "ok", "curtidas": c}
+    return JsonResponse(jt)
 
 @csrf_exempt
 def api_twips(request):
    if request.method == "GET":
       twips = Twip.objects.all()
+      u = User.objects.get(username='admin')
 
       jt = {"lista":[]} 
 
       for t in twips:
-         temp = {"id": t.id, "texto": t.texto, "autor": t.autor.username}
+         curtiu = t.curtida_set.filter(autor_id = u.id).count() > 0
+         temp = {"id": t.id, "texto": t.texto, "autor": t.autor.username, "curtidas": t.curtida_set.count(), "curtiu": curtiu}
          jt["lista"].append(temp)   
        
       return JsonResponse(jt)
@@ -34,10 +56,10 @@ def api_twips(request):
 
       #Salva no banco
       t = request.POST["texto"]
-      usuario = User.objects.get(username='admin')
-      twip = Twip(autor=usuario, texto = t)
+      u = User.objects.get(username='admin')
+      twip = Twip(autor=u, texto = t)
       twip.save()
       
-      jt = {"mensagem": "ok", "id": twip.id, "usuario": usuario.username}
+      jt = {"mensagem": "ok", "id": twip.id, "usuario": u.username}
       return JsonResponse(jt)
  
