3e35dc2a5a5adebdc02841cd1989594b90704b31
Arton Dorneles <artondorneles@gmail.com>
2018-10-29 16:32:13 -0200

    Adicionado login funciononal. Ainda não possui verificação de segurança em operações sensíveis

--- twipin/views.py
+++ twipin/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from .models import Twip,Curtida
+from .forms import FormEntrar
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, logout, login
 
 def index(request):
+   if not request.user.is_authenticated: 
+      return HttpResponseRedirect('entrar') 
+
    contexto = {} 
    return render(request, 'twipin/index.html', contexto)
+
+
+def entrar(request):
+   contexto = {} 
+   if request.method == 'POST':
+      form = FormEntrar(request.POST)
+      if form.is_valid():
+         username = form.cleaned_data['username']
+         password = form.cleaned_data['password']
+         usuario = authenticate(request, username=username, password=password)
+         if usuario is not None:
+            login(request, usuario)
+            return HttpResponseRedirect('/') 
+         
+   form = FormEntrar() 
+   contexto = {"form": form}
+   return render(request, 'twipin/entrar.html', contexto)
+
+
+def sair(request):
+   logout(request)
+   return HttpResponseRedirect('/') 
+
 
 @csrf_exempt
 def api_twips_id(request, id):
@@ -24,13 +52,13 @@ def api_coracao(request, id):
 
     if request.method == "POST":
       t = Twip.objects.get(id=id)
-      u = User.objects.get(username='admin')
+      u = request.user 
       curtida = Curtida(autor=u, twip=t)
       curtida.save()
       c = t.curtida_set.count()
     elif request.method == "DELETE":
       t = Twip.objects.get(id=id)
-      u = User.objects.get(username='admin')
+      u = request.user 
       curtida = Curtida.objects.get(autor=u, twip=t)
       curtida.delete()
       c = t.curtida_set.count()
@@ -42,13 +70,13 @@ def api_coracao(request, id):
 def api_twips(request):
    if request.method == "GET":
       twips = Twip.objects.all()
-      u = User.objects.get(username='admin')
+      u = request.user 
 
       jt = {"lista":[]} 
 
       for t in twips:
          curtiu = t.curtida_set.filter(autor_id = u.id).count() > 0
-         temp = {"id": t.id, "texto": t.texto, "autor": t.autor.username, "curtidas": t.curtida_set.count(), "curtiu": curtiu}
+         temp = {"id": t.id, "texto": t.texto, "nome":t.autor.first_name, "autor": t.autor.username, "curtidas": t.curtida_set.count(), "curtiu": curtiu}
          jt["lista"].append(temp)   
        
       return JsonResponse(jt)
@@ -56,10 +84,10 @@ def api_twips(request):
 
       #Salva no banco
       t = request.POST["texto"]
-      u = User.objects.get(username='admin')
+      u = request.user 
       twip = Twip(autor=u, texto = t)
       twip.save()
       
-      jt = {"mensagem": "ok", "id": twip.id, "usuario": u.username}
+      jt = {"mensagem": "ok", "id": twip.id, "usuario": u.username, "nome": u.first_name}
       return JsonResponse(jt)
  
